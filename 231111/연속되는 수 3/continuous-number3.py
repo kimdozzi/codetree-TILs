84e707fdf2678b6feac688841f89fd9a0f9371e0
DoHyung Kim <neo5188@gmail.com>
2023-11-11 16:11:17 +0900

    Title: 연속되는 수 3, Time: 114ms, Memory: 24MB, Status: Passed - CodeTree

--- 231111/연속되는 수 3/continuous-number3.py
+++ 231111/연속되는 수 3/continuous-number3.py
@@ -2,25 +2,17 @@ import sys
 si = sys.stdin.readline
 n = int(si())
 lst = [int(si()) for _ in range(n)]
+arr = [1] * len(lst)
 
-ans = 0
-cnt = 1
-flag = False 
 for i in range(1,len(lst)) :
-    if lst[i] > 0 and lst[i-1] > 0 :
-        if not flag :
-            flag = True
-            ans = max(ans, cnt)
-            cnt = 1
-        else :
-            cnt += 1
+    if lst[i] < 0 and lst[i-1] < 0 :
+        arr[i] += arr[i-1]
     
-    elif lst[i] < 0 and lst[i-1] < 0 :
-        if flag :
-            flag = False
-            ans = max(ans, cnt)
-            cnt = 1
-        else :
-            cnt += 1
+    elif lst[i] > 0 and lst[i-1] > 0 :
+        arr[i] += arr[i-1]
 
-print(ans)
+
+mx = 0
+for i in arr :
+    mx = max(mx, i)
+print(mx)
