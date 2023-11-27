1b811807caaa1127b2dcb33a368e0d0b10391447
DoHyung Kim <neo5188@gmail.com>
2023-11-28 08:54:33 +0900

    Title: 좌우로 움직이는 로봇, Time: 123ms, Memory: 40MB, Status: Wrong Answer - CodeTree

---
+++ 231128/좌우로 움직이는 로봇/robot-moving-from-side-to-side.py
@@ -0,0 +1,29 @@
+import sys
+si = sys.stdin.readline
+
+MAX_T = 1000000
+
+n,m=tuple(map(int, si().split()))
+pos_a, pos_b = [0] * (MAX_T + 1), [0] * (MAX_T + 1)
+
+# A가 매 초마다 서있는 위치를 기록
+time_a = 1
+for _ in range(n): 
+    t,d = tuple(si().split())
+    for _ in range(int(t)) :
+        pos_a[time_a] = pos_a[time_a - 1] + (1 if d == 'R' else -1)
+        time_a += 1
+
+time_b = 1
+for _ in range(m) :
+    t,d = tuple(si().split())
+    for _ in range(int(t)) :
+        pos_b[time_b] = pos_b[time_b - 1] + (1 if d == 'R' else -1)
+        time_b += 1
+        
+ans = 0
+for i in range(MAX_T+1) :
+    if pos_a[i] == pos_b[i] and pos_a[i-1] != pos_b[i-1] :
+        ans += 1
+
+print(ans)
