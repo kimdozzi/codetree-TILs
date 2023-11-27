60683563ae22811e31e367364073bfc70706ec39
DoHyung Kim <neo5188@gmail.com>
2023-11-28 08:56:02 +0900

    Title: 좌우로 움직이는 로봇, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 231128/좌우로 움직이는 로봇/robot-moving-from-side-to-side.py
+++ 231128/좌우로 움직이는 로봇/robot-moving-from-side-to-side.py
@@ -20,7 +20,15 @@ for _ in range(m) :
     for _ in range(int(t)) :
         pos_b[time_b] = pos_b[time_b - 1] + (1 if d == 'R' else -1)
         time_b += 1
-        
+
+if time_a < time_b :
+    for i in range(time_a, time_b) :
+        pos_a[i] = pos_a[i-1]
+ellif time_a > time_b :
+    for i in range(time_b, time_a) :
+        pos_b[i] = pos_b[i-1]
+
+
 ans = 0
 for i in range(MAX_T+1) :
     if pos_a[i] == pos_b[i] and pos_a[i-1] != pos_b[i-1] :
