0087f83829b543679492a9abdc9ef5829d5dfee0
DoHyung Kim <neo5188@gmail.com>
2023-11-28 09:43:07 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 113ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 231128/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
@@ -0,0 +1,34 @@
+# N명의 개발자, T번에 걸쳐 t초에 x개발자가 y개발자와 악수를 나눔 
+# k번의 악수동안 전염병을 옮김, 개발자 번호 p 
+
+import sys
+si = sys.stdin.readline
+
+n,k,p,tc = map(int, si().split())
+
+people = [-1] * (n+1)
+people[k] = p
+
+data = []
+for _ in range(tc) :
+    t,x,y = map(int,si().split())
+    data.append((t,x,y))
+data.sort()
+
+for _,x,y in data :
+    if people[x] > 0 and people[y] == -1 :
+        people[y] = k
+        people[x] -= 1
+    
+    elif people[y] > 0 and people[x] == -1 :
+        people[x] = k
+        people[y] -= 1
+
+ans = ''
+for i in people[1:] :
+    if i >= 0 :
+        ans+='1'
+    else :
+        ans+='0'
+
+print(ans)
