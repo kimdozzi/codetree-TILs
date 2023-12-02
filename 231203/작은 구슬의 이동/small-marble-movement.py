b993885a3dc06f81fcab42e335b473963e8f08a4
DoHyung Kim <neo5188@gmail.com>
2023-12-03 08:57:44 +0900

    Title: 작은 구슬의 이동, Time: 119ms, Memory: 24MB, Status: Passed - CodeTree

--- 231203/작은 구슬의 이동/small-marble-movement.py
+++ 231203/작은 구슬의 이동/small-marble-movement.py
@@ -7,20 +7,16 @@ dic = ['R','D','U','L']
 n,t=map(int,si().split())
 x,y,direction = si().split()
 x,y=int(x)-1,int(y)-1
-board=[[0]*n for _ in range(n)]
-board[x][y] = 1
 
 def is_range(nx,ny) :
-    return 0 <=nx < n and 0 <=ny < n
+    return 0 <= nx < n and 0 <= ny < n
 
 d = dic.index(direction)
-for tt in range(1,t+1) :
+for _ in range(t) :
     nx, ny = dxs[d] + x, dys[d] + y
     if not is_range(nx,ny) :
         d = 3 - d
         continue
-    board[x][y] = 0
-    board[nx][ny] = 1
     x,y = nx,ny
 
 print(x+1,y+1)
