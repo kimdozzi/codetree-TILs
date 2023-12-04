5527836a1c00bcd87049a257e128ac1c7edacff4
DoHyung Kim <neo5188@gmail.com>
2023-12-04 11:02:49 +0900

    Title: 빙빙 돌며 숫자 사각형 채우기 2, Time: 179ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 231204/빙빙 돌며 숫자 사각형 채우기 2/snail-number-square-2.py
@@ -0,0 +1,26 @@
+import sys
+si = sys.stdin.readline 
+
+def in_range(x,y) :
+    return 0<=x<r and 0<=y<c
+
+dirs = ((1,0),(0,1),(-1,0),(0,-1))
+
+r,c=map(int,si().split())
+board=[[0] * c for _ in range(r)]
+x,y = 0,0
+board[x][y] = 1
+num = 2
+direction = 0
+while num <= r*c :
+    nx,ny = x+dirs[direction][0], y+dirs[direction][1]
+    if in_range(nx,ny) and board[nx][ny] == 0 :
+        board[nx][ny] = num
+        num += 1
+        x,y=nx,ny
+        
+    else :
+        direction = (direction + 1 + 4) % 4
+
+for i in board:
+    print(*i, sep=' ')
