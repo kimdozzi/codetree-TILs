ee4973b9b245577339fffb542be8056cbe0e2daa
DoHyung Kim <neo5188@gmail.com>
2023-12-07 00:11:17 +0900

    Title: 이상한 진수 2, Time: 134ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 231207/이상한 진수 2/awkward-digits-2.py
+++ 231207/이상한 진수 2/awkward-digits-2.py
@@ -2,10 +2,17 @@ import sys
 si = sys.stdin.readline
 s=list(si().rstrip())
 
+flag = False
 for i in range(len(s)) :
     if s[i] == '0' :
         s[i] = '1'
+        flag = True
         break
+if not flag :
+    for i in range(len(s)-1,-1,-1) :
+        if s[i] == '1': 
+            s[i] = '0'
+            break
 
 l = len(s)-1
 res = 0
