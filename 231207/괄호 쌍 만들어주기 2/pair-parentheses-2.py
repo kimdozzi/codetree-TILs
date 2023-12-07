101e75e778df4d9416fda8eb0dc67c1f99d05edc
DoHyung Kim <neo5188@gmail.com>
2023-12-07 09:30:30 +0900

    Title: 괄호 쌍 만들어주기 2, Time: 114ms, Memory: 24MB, Status: Passed - CodeTree

--- 231207/괄호 쌍 만들어주기 2/pair-parentheses-2.py
+++ 231207/괄호 쌍 만들어주기 2/pair-parentheses-2.py
@@ -2,7 +2,7 @@ import sys
 si = sys.stdin.readline
 s=si().rstrip()
 res = 0
-for i in range(len(s)) :
+for i in range(len(s)-1) :
     if s[i] == '(' and s[i+1] == '(':
         for j in range(i+1,len(s)-1) :
             if s[j] ==')' and s[j+1]==')' :
