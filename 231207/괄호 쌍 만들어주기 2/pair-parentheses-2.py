a99763760cb7b8f44643efca0b33e85a8599734d
DoHyung Kim <neo5188@gmail.com>
2023-12-07 09:30:09 +0900

    Title: 괄호 쌍 만들어주기 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 231207/괄호 쌍 만들어주기 2/pair-parentheses-2.py
+++ 231207/괄호 쌍 만들어주기 2/pair-parentheses-2.py
@@ -3,9 +3,9 @@ si = sys.stdin.readline
 s=si().rstrip()
 res = 0
 for i in range(len(s)) :
-    if s[i] == '(' :
-        for j in range(i+1,len(s)) :
-            if s[j] ==')' :
+    if s[i] == '(' and s[i+1] == '(':
+        for j in range(i+1,len(s)-1) :
+            if s[j] ==')' and s[j+1]==')' :
                 res += 1
 
 print(res)
