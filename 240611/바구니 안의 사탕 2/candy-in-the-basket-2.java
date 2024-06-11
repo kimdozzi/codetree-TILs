d086401d767efcd6d912fc0e8cc518f68e9b2667
DoHyung Kim <neo5188@gmail.com>
2024-06-11 17:26:42 +0900

    Title: 바구니 안의 사탕 2, Time: 177ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240611/바구니 안의 사탕 2/candy-in-the-basket-2.java
+++ 240611/바구니 안의 사탕 2/candy-in-the-basket-2.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
 public class Main {
-    public static final int MAX_N = 101;
+    public static final int MAX_N = 10000;
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
