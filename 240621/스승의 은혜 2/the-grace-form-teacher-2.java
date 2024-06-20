f8fd28be47a81d1091b782d46b2199cc66c733f3
DoHyung Kim <neo5188@gmail.com>
2024-06-21 08:39:19 +0900

    Title: 스승의 은혜 2, Time: 121ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240621/스승의 은혜 2/the-grace-form-teacher-2.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int B = sc.nextInt();
+        int[] arr = new int[1001];
+        int maxCnt = 0;
+        
+        for(int i=0; i<N; i++) 
+            arr[i] = (int) (sc.nextInt() / 2);
+        
+        for (int i=0; i<N; i++) {
+            int sum = B;
+            int cnt = 0;
+            for (int j=0; j<N; j++) {
+                if (i==j) continue;
+
+                if(sum-arr[j] >= 0) {
+                    sum -= arr[j];
+                    cnt++;
+                }
+            }
+
+            maxCnt = Math.max(cnt, maxCnt);
+
+        }
+        System.out.print(maxCnt);
+    }
+}
