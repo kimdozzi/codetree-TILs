050036280ddf11f0a4702ec22a4158d7abe6f852
DoHyung Kim <neo5188@gmail.com>
2024-06-21 08:53:44 +0900

    Title: 스승의 은혜 2, Time: 149ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240621/스승의 은혜 2/the-grace-form-teacher-2.java
+++ 240621/스승의 은혜 2/the-grace-form-teacher-2.java
@@ -15,11 +15,11 @@ public class Main {
 
         Arrays.sort(arr);
         
-        for (int i=0; i<1001; i++) {
+        for (int i=0; i<1000; i++) {
             int sum = B;
             int cnt = 0;
             
-            for (int j=0; j<1001; j++) {
+            for (int j=0; j<1000; j++) {
                 if (i==j) {
                     if(arr[j] > 0 && sum-(int)(arr[j]/2) >= 0) {
                     sum -= (int)(arr[j]/2);
