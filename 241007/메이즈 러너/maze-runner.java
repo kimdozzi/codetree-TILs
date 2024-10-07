c012bded23dc57354a5e3f9cf3733c6ec690f82d
DoHyung Kim <neo5188@gmail.com>
2024-10-07 20:42:38 +0900

    Title: 메이즈 러너, Time: 109ms, Memory: 8MB, Status: Passed - CodeTree

--- 241007/메이즈 러너/maze-runner.java
+++ 241007/메이즈 러너/maze-runner.java
@@ -3,9 +3,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -13,9 +10,9 @@ public class Main {
 	static int[][] board; // 0:빈칸, 1~9 벽의 내구도 (0이되면 빈칸이 됨)
 	static int[][] dirs = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}}; // 상하좌우 우선순위
 	static int answer; // 모든 참가자들의 이동거리 합
-	static int[] entrance; // 출구 좌표
-	static boolean[] isOut; // 참가자의 탈출 여부
-	static Queue<int[]> peopleList; // 참가자들의 좌표
+	static Pair exits; // 출구 좌표
+	static Pair[] travelers;
+	static int sx, sy, squareSize;
 
 	public static void main(String[] args) throws IOException {
 		// init
@@ -34,50 +31,94 @@ public class Main {
 			}
 		}
 
-		peopleList = new LinkedList<>();
-		isOut = new boolean[M];
-
+		travelers = new Pair[M];
 		for (int i = 0; i < M; i++) {
 			st = new StringTokenizer(br.readLine());
 			int x = Integer.parseInt(st.nextToken());
 			int y = Integer.parseInt(st.nextToken());
-			peopleList.add(new int[] {x - 1, y - 1});
-			isOut[i] = false;
+			travelers[i] = new Pair(x - 1, y - 1);
 		}
+
 		st = new StringTokenizer(br.readLine());
 		int x = Integer.parseInt(st.nextToken()) - 1;
 		int y = Integer.parseInt(st.nextToken()) - 1;
-		entrance = new int[] {x, y};
+		exits = new Pair(x, y);
 
 		for (int t = 1; t <= K; t++) {
-			// 1. 먼저 보드에 아직 탈출하지 못한 참가자와 출구를 표시합니다.
-			makeBoard();
 
-			// 2. 각 참가자들을 동시에 출구와 가까운 쪽으로 이동시킵니다.
+			// 1. 각 참가자들을 동시에 출구와 가까운 쪽으로 이동시킵니다.
 			move();
 
-			// 3. 한 명 이상의 참가자와 출구를 포함한 가장 작은 정사각형을 돌리기 위해 기준이 되는 좌표 구합니다.
-			int[] pos = findPos();
+			// 모든 사람이 출구로 탈출했는지 판단합니다.
+			boolean isAllEscaped = true;
+			for (int i = 0; i < M; i++) {
+				if (!(travelers[i].x == exits.x && travelers[i].y == exits.y)) {
+					isAllEscaped = false;
+				}
+			}
+			// 만약 모든 사람이 출구로 탈출했으면 바로 종료합니다.
+			if (isAllEscaped)
+				break;
 
-			rotate90(pos[0], pos[1], pos[2]);
+			// 2. 한 명 이상의 참가자와 출구를 포함한 가장 작은 정사각형을 돌리기 위해 기준이 되는 좌표 구합니다.
+			findPos();
 
+			// 3. 정사각형 내부의 벽을 회전시킵니다.
+			rotateSquare();
+
+			// 4. 모든 참가자들 및 출구를 회전시킵니다.
+			rotateTravelerAndExit();
 
 		}
 		System.out.println(answer);
-		System.out.println((entrance[0] + 1) + " " + (entrance[1] + 1));
+		System.out.println((exits.x + 1) + " " + (exits.y + 1));
 	}
 
-	private static void rotate90(int sx, int sy, int length) {
+	// 모든 참가자들 및 출구를 회전시킵니다.
+	public static void rotateTravelerAndExit() {
+		// m명의 참가자들을 모두 확인합니다.
+		for (int i = 0; i < M; i++) {
+			int x = travelers[i].x;
+			int y = travelers[i].y;
+			// 해당 참가자가 정사각형 안에 포함되어 있을 때에만 회전시킵니다.
+			if (sx <= x && x < sx + squareSize && sy <= y && y < sy + squareSize) {
+				// Step 1. (sx, sy)를 (0, 0)으로 옮겨주는 변환을 진행합니다.
+				int ox = x - sx, oy = y - sy;
+
+				// Step 2. 변환된 상태에서는 회전 이후의 좌표가 (x, y) -> (y, squareN - x - 1)가 됩니다.
+				int rx = oy, ry = squareSize - ox - 1;
+
+				// Step 3. 다시 (sx, sy)를 더해줍니다.
+				travelers[i].x = rx + sx;
+				travelers[i].y = ry + sy;
+			}
+		}
+
+		// 출구에도 회전을 진행합니다.
+		int x = exits.x;
+		int y = exits.y;
+		if (sx <= x && x < sx + squareSize && sy <= y && y < sy + squareSize) {
+			// Step 1. (sx, sy)를 (0, 0)으로 옮겨주는 변환을 진행합니다.
+			int ox = x - sx, oy = y - sy;
+			// Step 2. 변환된 상태에서는 회전 이후의 좌표가 (x, y) -> (y, squareN - x - 1)가 됩니다.
+			int rx = oy, ry = squareSize - ox - 1;
+			// Step 3. 다시 (sx, sy)를 더해줍니다.
+			exits.x = rx + sx;
+			exits.y = ry + sy;
+		}
+	}
+
+	private static void rotateSquare() {
 
 		int[][] tempBoard = new int[N][N];
 
-		for (int x = sx; x < sx + length; x++) {
-			for (int y = sy; y < sy + length; y++) {
+		for (int x = sx; x < sx + squareSize; x++) {
+			for (int y = sy; y < sy + squareSize; y++) {
 				// 1. (0,0)으로 옮깁니다.
 				int ox = x - sx, oy = y - sy;
 
 				// 2. 90도 회전했을 때의 좌표로 위치를 옮깁니다.
-				int rx = oy, ry = length - ox - 1;
+				int rx = oy, ry = squareSize - ox - 1;
 
 				// 3. 다시 원래 좌표로 바꿉니다.
 				int nx = sx + rx, ny = sy + ry;
@@ -85,131 +126,103 @@ public class Main {
 			}
 		}
 
-		for (int x = sx; x < sx + length; x++) {
-			for (int y = sy; y < sy + length; y++) {
+		for (int x = sx; x < sx + squareSize; x++) {
+			for (int y = sy; y < sy + squareSize; y++) {
 				board[x][y] = tempBoard[x][y];
 				// 벽의 내구도를 1 내립니다.
 				if (board[x][y] > 0)
 					board[x][y] = Math.max(board[x][y] - 1, 0);
 			}
 		}
-
-		// 돌리고 난 후 입구 정보와 참가자의 정보를 갱신해줍니다.
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < N; j++) {
-				if (board[i][j] == -1) {
-					peopleList.add(new int[] {i, j});
-				} else if (board[i][j] == -2) {
-					entrance[0] = i;
-					entrance[1] = j;
-				}
-			}
-		}
 	}
 
-	private static int[] findPos() {
-		int size = 2; // 2*2 정사각형부터 10*10(최대) 사이즈까지 완전탐색을 수행합니다.
-		boolean isExistPeople = false;
-		boolean isExistEntrance = false;
+	private static void findPos() {
+		// 가장 작은 정사각형부터 모든 정사각형을 만들어봅니다.
+		for (int sz = 2; sz <= N; sz++) {
+			// 가장 좌상단 r 좌표가 작은 것부터 하나씩 만들어봅니다.
+			for (int x1 = 0; x1 < N - sz + 1; x1++) {
+				// 가장 좌상단 c 좌표가 작은 것부터 하나씩 만들어봅니다.
+				for (int y1 = 0; y1 < N - sz + 1; y1++) {
+					int x2 = x1 + sz - 1;
+					int y2 = y1 + sz - 1;
 
-		while (size <= 10) {
-			for (int i = 0; i < N - size + 1; i++) {
-				for (int j = 0; j < N - size + 1; j++) {
-					for (int r = i; r < i + size; r++) {
-						for (int c = j; c < j + size; c++) {
-							if (board[r][c] == -1) {
-								isExistPeople = true;
-							}
-							if (board[r][c] == -2) {
-								isExistEntrance = true;
+					// 만약 출구가 해당 정사각형 안에 없다면 스킵합니다.
+					if (!(x1 <= exits.x && exits.x <= x2 && y1 <= exits.y && exits.y <= y2)) {
+						continue;
+					}
+
+					// 한 명 이상의 참가자가 해당 정사각형 안에 있는지 판단합니다.
+					boolean isTravelerIn = false;
+					for (int l = 0; l < M; l++) {
+						if (x1 <= travelers[l].x && travelers[l].x <= x2 && y1 <= travelers[l].y
+							&& travelers[l].y <= y2) {
+							// 출구에 있는 참가자는 제외합니다.
+							if (!(travelers[l].x == exits.x && travelers[l].y == exits.y)) {
+								isTravelerIn = true;
 							}
 						}
 					}
-					if (isExistEntrance && isExistPeople) {
-						return new int[] {i, j, size};
-					} else {
-						isExistPeople = false;
-						isExistEntrance = false;
-					}
 
+					// 만약 한 명 이상의 참가자가 해당 정사각형 안에 있다면
+					// sx, sy, sqaureSize 정보를 갱신하고 종료합니다.
+					if (isTravelerIn) {
+						sx = x1;
+						sy = y1;
+						squareSize = sz;
+
+						return;
+					}
 				}
 			}
-			size++;
 		}
-
-		return new int[] {0, 0, 0};
-	}
-
-	private static void printPeoplePosition() {
-		Iterator<int[]> iterator = peopleList.iterator();
-		while (iterator.hasNext()) {
-			int[] next = iterator.next();
-			System.out.println("위치: " + next[0] + " " + next[1]);
-		}
-		System.out.println();
-	}
-
-	private static void printBoard(int[][] board) {
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < N; j++) {
-				System.out.print(board[i][j] + " ");
-			}
-			System.out.println();
-		}
-		System.out.println();
 	}
 
 	private static void move() {
-		int ex = entrance[0];
-		int ey = entrance[1];
+		// m명의 모든 참가자들에 대해 이동을 진행합니다.
+		for (int i = 0; i < M; i++) {
+			// 이미 출구에 있는 경우 스킵합니다.
+			if (travelers[i].x == exits.x && travelers[i].y == exits.y)
+				continue;
 
-		Queue<int[]> q = new LinkedList<>();
-		while (!peopleList.isEmpty()) {
-			q.add(peopleList.poll());
-		}
+			// 행이 다른 경우 행을 이동시켜봅니다.
+			if (travelers[i].x != exits.x) {
+				int nx = travelers[i].x;
+				int ny = travelers[i].y;
 
-		while (!q.isEmpty()) {
-			int[] xyPos = q.poll();
+				if (exits.x > nx)
+					nx++;
+				else
+					nx--;
 
-			int x = xyPos[0];
-			int y = xyPos[1];
-
-			for (int d = 0; d < 4; d++) {
-				int nx = x + dirs[d][0];
-				int ny = y + dirs[d][1];
-
-				// 참가자가 탈출구에 도착했다면
-				// 이전 위치는 빈칸(0)으로 만들고, 탈출 여부를 표시합니다.
-				// 그리고, 나머지 단계는 스킵합니다.
-				if (nx == entrance[0] && ny == entrance[1]) {
+				// 벽이 없다면 행을 이동시킬 수 있습니다.
+				// 이 경우 행을 이동시키고 바로 다음 참가자로 넘어갑니다.
+				if (board[nx][ny] == 0) {
+					travelers[i].x = nx;
+					travelers[i].y = ny;
 					answer++;
-					board[x][y] = 0;
 					continue;
-				}
-
-				// 격자를 벗어난다면 continue
-				if (!inRange(nx, ny))
-					continue;
-
-				// 해당 위치가 벽이라면 continue;
-				if (1 <= board[nx][ny] && board[nx][ny] <= 9)
-					continue;
-
-				// 기존 위치에서 출구 까지의 거리와 이동하려는 위치에서 출구 까지의 거리를 구합니다.
-				int originalDist = Math.abs(x - ex) + Math.abs(y - ey);
-				int MoveDist = Math.abs(nx - ex) + Math.abs(ny - ey);
-
-				// 기존 거리보다 이동할 거리의 거리가 짧다면, 해당 위치로 이동합니다.
-				if (MoveDist < originalDist) {
-					// 맵에 반영.
-					board[x][y] = 0;
-					board[nx][ny] = -1;
-					answer++;
-					//peopleList.add(new int[] {nx, ny});
-					break;
 				}
 			}
-			//peopleList.add(new int[] {x, y});
+
+			// 열이 다른 경우 열을 이동시켜봅니다.
+			if (travelers[i].y != exits.y) {
+				int nx = travelers[i].x;
+				int ny = travelers[i].y;
+
+				if (exits.y > ny)
+					ny++;
+				else
+					ny--;
+
+				// 벽이 없다면 행을 이동시킬 수 있습니다.
+				// 이 경우 열을 이동시킵니다.
+				if (board[nx][ny] == 0) {
+					travelers[i].x = nx;
+					travelers[i].y = ny;
+					answer++;
+					continue;
+				}
+			}
 		}
 	}
 
@@ -217,15 +230,14 @@ public class Main {
 		return 0 <= nx && nx < N && 0 <= ny && ny < N;
 	}
 
-	private static void makeBoard() {
-		// 탈출하지 못한 참가자 표시
-		Iterator<int[]> iterator = peopleList.iterator();
-		while (iterator.hasNext()) {
-			int[] next = iterator.next();
-			board[next[0]][next[1]] = -1;
-		}
+	static class Pair {
+		int x;
+		int y;
 
-		// 입구 표시
-		board[entrance[0]][entrance[1]] = -2;
+		public Pair(int x, int y) {
+			this.x = x;
+			this.y = y;
+		}
 	}
+
 }
