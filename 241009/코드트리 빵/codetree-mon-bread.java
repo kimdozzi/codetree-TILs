41b4f74f7d1558708ad09bbc0cb1280b669dc2aa
DoHyung Kim <neo5188@gmail.com>
2024-10-09 12:00:53 +0900

    Title: 코드트리 빵, Time: 151ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 241009/코드트리 빵/codetree-mon-bread.java
+++ 241009/코드트리 빵/codetree-mon-bread.java
@@ -204,7 +204,7 @@ public class Main {
 				if (current.x == ex && current.y == ey) {
 					Pair next = current;
 					while (parent[next.x][next.y] != null && (
-						parent[next.x][next.y].x != x || parent[next.x][next.y].y != y)) {
+						parent[next.x][next.y].x != x && parent[next.x][next.y].y != y)) {
 						next = parent[next.x][next.y];
 					}
 					people[idx] = next;
@@ -218,7 +218,7 @@ public class Main {
 					if (!inRange(nx, ny) || vis[nx][ny] || cannotMove[nx][ny])
 						continue;
 
-					q.offer(new Pair(nx, ny));
+					q.add(new Pair(nx, ny));
 					vis[nx][ny] = true;
 					parent[nx][ny] = current;
 				}
